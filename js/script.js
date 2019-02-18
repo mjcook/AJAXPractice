0192eddabaae926a16f5a44b30c5fc25bf1fdd20
Mitchell Cook <mitchellcookofficial@gmail.com>
2019-02-17 17:18:37 -0700

    Updated to add wikipedia and new york times ajax

--- js/script.js
+++ js/script.js
@@ -11,59 +11,55 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var streetStr = $('#street').val();
-    var cityStr = $('#city').val();
-    var address = streetStr + ', ' + cityStr;
-
-    $greeting.text('So, you want to live at ' + address + '?');
-
     // load streetview
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
+    var street = $('#street').val();
+    var city = $('#city').val();
+    var imgSrc = 'http://maps.googleapis.com/maps/api/streetview?size=600x300&location=' + street + ', ' + city;
 
+    //$('body').append('<img class="bgimg" src="'+ imgSrc + '">'); NOT VALID WITHOUT API KEY
+    var nytAPI = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + city + '&sort=newest&api-key=TpMKpyLufUcZ2L9ypvSFjBCR0Tl55dLN';
 
-    // load nytimes
-    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=3abc9a3d23e60b38c21b4ab9b0a91c07:17:69911633'
-    $.getJSON(nytimesUrl, function(data){
-
-        $nytHeaderElem.text('New York Times Articles About ' + cityStr);
-
-        articles = data.response.docs;
-        for (var i = 0; i < articles.length; i++) {
-            var article = articles[i];
-            $nytElem.append('<li class="article">'+
-                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
-                '<p>' + article.snippet + '</p>'+
-            '</li>');
-        };
-
-    }).error(function(e){
-        $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
+    $.getJSON(nytAPI, function(data)
+    {
+        var docs = data["response"]["docs"];
+        var $nytArtList = $('#nytimes-articles');
+        for (docIndex in docs){
+            var $articleElem = $('<li class="article"></li>');
+            var $attrElem = $('<a href="'+docs[docIndex]['web_url']+'">'+docs[docIndex]['headline']['main']+'</a>')
+            var $parElem = $('<p>'+docs[docIndex]['lead_paragraph']+'</p>')
+            $articleElem.append($attrElem);
+            $articleElem.append($parElem);
+            $nytArtList.append($articleElem);
+        }
+    }).fail(function(e){
+        $nytHeaderElem.text('NYT Articles Cannot Be Loaded');
     });
 
+    var wikiAPI = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=' + city + '&gsrlimit=15&callback=JSON_CALLBACK';
 
-
-    // load wikipedia data
-    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
-    var wikiRequestTimeout = setTimeout(function(){
-        $wikiElem.text("failed to get wikipedia resources");
+    var wikiTimeout = setTimeout(function(){
+        $wikiElem.text("Wiki Resources Cannot Be Loaded");
     }, 8000);
 
     $.ajax({
-        url: wikiUrl,
-        dataType: "jsonp",
+        url: wikiAPI,
         jsonp: "callback",
-        success: function( response ) {
-            var articleList = response[1];
-
-            for (var i = 0; i < articleList.length; i++) {
-                articleStr = articleList[i];
-                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-            };
-
-            clearTimeout(wikiRequestTimeout);
+        dataType: 'jsonp',
+        xhrFields: {
+            withCredentials: true
         }
+    }).done(function(data) {
+        console.log(data);
+        var pages = data["query"]["pages"];
+        var $wikiArtList = $('#wikipedia-links');
+        for (pageIndex in pages){
+            var $articleElem = $('<li></li>');
+            var $attrElem = $('<a href="' + 'http://en.wikipedia.org/?curid=' + pages[pageIndex]['pageid'] + '">'+pages[pageIndex]['title']+'</a>');
+            $articleElem.append($attrElem);
+            $wikiArtList.append($articleElem);
+        }
+
+        clearTimeout(wikiTimeout);
     });
 
     return false;
